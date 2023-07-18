9f17bfc7d0d346174ff23cdf261106c6ecdf3190
saral-saxena <saral.saxena@tide.co>
2023-07-18 17:46:03 +0530

    full project

---
+++ src/main/java/com/saral/dsa/DsaSvcApplication.java
@@ -0,0 +1,13 @@
+package com.saral.dsa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DsaSvcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DsaSvcApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/saral/dsa/DsaSvcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.saral.dsa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DsaSvcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
